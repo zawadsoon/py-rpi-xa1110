11bff18c41100176d065eaaa5302d5f3ba113391
Marcin Zawada <zawadsoon@gmail.com>
2018-09-10 18:43:33 +0000

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='py-rpi-xa1110',
+    version='0.1',
+    description='A bunch of code to interfecae between RPI and SparkFun XA1110',
+    author='Marcin Zawada',
+    author_email='zawadsoon@gmail.com',
+    url='https://github.com/zawadsoon/py-rpi-xa1110',
+)
